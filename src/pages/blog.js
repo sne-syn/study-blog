7174e7f3ebb45f3f26a29f4508525f03be651e0b
sne-syn <36764312+sne-syn@users.noreply.github.com>
2021-02-08 19:49:24 +0100

    Create blog.js

---
+++ src/pages/blog.js
@@ -0,0 +1,102 @@
+
+
+import React from 'react'
+
+const BlogPage = () =>  (
+ <body class="page-blog">
+    <div class="article-grid">
+      <article class="article-preview article-preview--featured">
+        <img src="img/blog-1.svg" alt="electric drill">
+        <h3>
+          <a href="#">Lorem ipsum dolor sit amet, consectet adipiscing</a>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consectet adipiscing</p>
+        <div class="info">
+          <time datetime="2020-10-08">Oct 8</time>
+          <p>8 min read</p>
+        </div>
+      </article>
+      <article class="article-preview flow-content">
+        <img src="img/blog-2.svg" alt="electric drill">
+        <h3>
+          <a href="#">Lorem ipsum dolor sit amet, consectet adipiscing</a>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consectet adipiscing</p>
+        <div class="info">
+          <time datetime="2020-10-08">Oct 8</time>
+          <p>8 min read</p>
+        </div>
+      </article>
+      <article class="article-preview flow-content">
+        <img src="img/blog-3.svg" alt="electric drill">
+        <h3>
+          <a href="#">Lorem ipsum dolor sit amet, consectet adipiscing</a>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consectet adipiscing</p>
+        <div class="info">
+          <time datetime="2020-10-08">Oct 8</time>
+          <p>8 min read</p>
+        </div>
+      </article>
+      <article class="article-preview flow-content">
+        <img src="img/blog-4.svg" alt="electric drill">
+        <h3>
+          <a href="#">Lorem ipsum dolor sit amet, consectet adipiscing</a>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consectet adipiscing</p>
+        <div class="info">
+          <time datetime="2020-10-08">Oct 8</time>
+          <p>8 min read</p>
+        </div>
+      </article>
+      <article class="article-preview flow-content">
+        <img src="img/blog-5.svg" alt="electric drill">
+        <h3>
+          <a href="#">Lorem ipsum dolor sit amet, consectet adipiscing</a>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consectet adipiscing</p>
+        <div class="info">
+          <time datetime="2020-10-08">Oct 8</time>
+          <p>8 min read</p>
+        </div>
+      </article>
+      <article class="article-preview flow-content">
+        <img src="img/blog-6.svg" alt="electric drill">
+        <h3>
+          <a href="#">Lorem ipsum dolor sit amet, consectet adipiscing</a>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consectet adipiscing</p>
+        <div class="info">
+          <time datetime="2020-10-08">Oct 8</time>
+          <p>8 min read</p>
+        </div>
+      </article>
+      <article class="article-preview flow-content">
+        <img src="img/blog-7.svg" alt="electric drill">
+        <h3>
+          <a href="#">Lorem ipsum dolor sit amet, consectet adipiscing</a>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consectet adipiscing</p>
+        <div class="info">
+          <time datetime="2020-10-08">Oct 8</time>
+          <p>8 min read</p>
+        </div>
+      </article>
+      <article class="article-preview flow-content">
+        <img src="img/blog-8.svg" alt="electric drill">
+        <h3>
+          <a href="#">Lorem ipsum dolor sit amet, consectet adipiscing</a>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consectet adipiscing</p>
+        <div class="info">
+          <time datetime="2020-10-08">Oct 8</time>
+          <p>8 min read</p>
+        </div>
+      </article>
+    </div>
+  </div
+  </body>
+)
+
+
+export default BlogPage
