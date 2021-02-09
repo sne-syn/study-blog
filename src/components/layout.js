f30b9d75434dbac0a1c96f2c138f31fbc4bc03c6
sne-syn <36764312+sne-syn@users.noreply.github.com>
2021-02-09 21:02:54 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
 import Footer from "./footer"
 import EmailSubscribe from './email-subscribe'
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <Header />
